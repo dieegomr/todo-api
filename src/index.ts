6a901425dae08d1c03023551c04ecfd09207591d
dieegomr <dmendes.rocha@gmail.com>
2023-01-25 15:31:04 -0300

    feat: add express initial server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('Test');
+import express from 'express';
+import { config } from 'dotenv';
+
+config();
+
+const app = express();
+
+const port = process.env.port || 8000;
+
+app.get('/', (req, res) => res.send('Working'));
+
+app.listen(port, () => console.log(`listening on port ${port}`));
